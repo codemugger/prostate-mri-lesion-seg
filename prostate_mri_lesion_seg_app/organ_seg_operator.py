3b6f1f9653b5121919064ed746094c28b0c6a2f2
codemugger <ansontay6463@gmail.com>
2025-12-11 01:07:45 +0800

    Adjusted organ_Seg_operator post processing to ensure multi-class output for organ segmentation for TZ, PZ, and background.

--- prostate_mri_lesion_seg_app/organ_seg_operator.py
+++ prostate_mri_lesion_seg_app/organ_seg_operator.py
@@ -192,7 +192,7 @@ class ProstateSegOperator(Operator):
                     to_tensor=True,
                 ),
                 DataStatsd(keys=pred_key, name="Inverted output"),
-                AsDiscreted(keys=pred_key, argmax=True, threshold=0.5),
+                AsDiscreted(keys=pred_key, argmax=True, threshold=None),
                 DataStatsd(keys=pred_key, name="AsDiscrete output")
             ]
         )
